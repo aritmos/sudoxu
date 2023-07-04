736d8f22acba687bcf6f20d99f5c4b92d24eb930
aritmos <52433836+aritmos@users.noreply.github.com>
2023-07-04 13:06:31 +0100

    add lib.rs
    added lib.rs to have easier access to functions for assembly output viewing

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+#![allow(incomplete_features)]
+#![feature(slice_flatten)]
+#![feature(generic_const_exprs)]
+#![feature(maybe_uninit_array_assume_init)]
+
+mod structs;
+pub use structs::*;
+
+mod utils;
+pub use utils::*;
