3b885433058dd87cd904490bbbaf325d3c0fbaa3
aritmos <52433836+aritmos@users.noreply.github.com>
2023-07-14 01:44:34 +0200

    rewrite `Filter`
    Rewrote filter so each `Filter` only refers to a single index. Thought this was a cleaner and more performant implementation.

--- src/filters/filter_known.rs
+++ src/filters/filter_known.rs
@@ -1,11 +1,11 @@
 use crate::structs::*;
-use std::mem::transmute;
+use std::mem::{transmute, MaybeUninit};
 
 impl Cell {
     /// Returns `Some(n)` if the cell has a known `Num` "`n`" else None.
     /// # Safety
     /// Does no error checking to verify cell is correct.
-    /// Simply looks at the "known" byte and counts ones.
+    /// Simply looks at the "known" byte and counts toggled bits.
     pub fn known(&self) -> Option<Num> {
         let cell_u16 = self.to_u16();
         if cell_u16 % 2 != 1 {
@@ -20,16 +20,25 @@ impl Cell {
 }
 
 impl Grid {
-    pub fn known(&mut self, idx: GridIdx) -> Filter {
-        let Some(num) = self.get(idx).known() else {
-            panic!("Tried to use `filter_known` on an unknown cell.");
-        };
-        let idxs = {
+    /// Creates filters for all neighboring `Cell`s of a newly found known `Cell`
+    /// # Safety
+    /// The caller must ensure that the provided `Cell` at the given index is "known"
+    /// Panics if not a known `Cell`
+    pub fn get_known_filters(&mut self, idx: GridIdx) -> [Filter; 24] {
+        let num = self.get(idx).known().unwrap();
+        let mask = num.to_neg_mask();
+
+        let idxs: [GridIdx; 24] = {
             let comp_idxs_nested = Grid::compliment_indices(idx);
-            let comp_idxs_flat = unsafe { transmute::<_, [Idx<81>; 24]>(comp_idxs_nested) };
-            Vec::from(comp_idxs_flat)
+            unsafe { transmute(comp_idxs_nested) }
         };
 
-        Filter::new(num.to_mask(), idxs)
+        let mut filters: [MaybeUninit<Filter>; 24] = MaybeUninit::uninit_array();
+
+        for (idx, uninit_filter) in filters.iter_mut().enumerate() {
+            *uninit_filter = MaybeUninit::new(Filter::new(mask, idxs[idx]));
+        }
+
+        unsafe { MaybeUninit::array_assume_init(filters) }
     }
 }
--- src/structs/filter.rs
+++ src/structs/filter.rs
@@ -4,40 +4,58 @@ use crate::structs::*;
 /// in certain contexts.
 pub type Mask = Cell;
 
+#[derive(Clone, Copy)]
 pub struct Filter {
-    mask: Mask,
-    idxs: Vec<GridIdx>,
+    pub mask: Mask,
+    pub idx: GridIdx,
 }
 
 impl Num {
+    /// Creates a `Mask` from a `Num`.
+    /// The `Mask` has `1`s except at the given index.
+    /// # Example
+    /// ```rust
+    /// let n = Num::from_unchecked(4);
+    /// let mask = n.to_mask();
+    /// assert_eq!(mask, 0b000000_000_001_000_0);
+    /// ```
+    #[inline]
     pub fn to_mask(self) -> Mask {
+        unsafe { Mask::new_unchecked(1 << self as u8) }
+    }
+
+    /// Creates a negative `Mask` from a `Num`.
+    /// The `Mask` has `1`s except at the given index.
+    /// # Example
+    /// ```rust
+    /// let n = Num::from_unchecked(4);
+    /// let mask = n.to_neg_mask();
+    /// assert_eq!(mask, 0b111111_111_110_111_1);
+    /// ```
+    /// # Safety
+    /// The caller guarantees that the produced `Mask` is only used on `Cell`s
+    /// via bitwise `AND`, such that prohibitted bits never get set to `1`.
+    #[inline]
+    pub fn to_neg_mask(self) -> Mask {
         unsafe { Mask::new_unchecked(!(1 << self as u8)) }
     }
 }
 
 impl Filter {
-    pub fn new(mask: Mask, idxs: Vec<GridIdx>) -> Self {
-        Self { mask, idxs }
+    pub fn new(mask: Mask, idx: GridIdx) -> Self {
+        Self { mask, idx }
     }
 }
 
 impl Grid {
     pub fn filter(&mut self, filter: Filter) {
-        for idx in filter.idxs {
-            let cell = self.get_mut(idx);
-            cell.mask(filter.mask);
-        }
+        let cell = self.get_mut(filter.idx);
+        *cell &= filter.mask;
     }
 
-    pub fn filter_multiple(&mut self, filters: Vec<Filter>) {
+    pub fn filter_multiple(&mut self, filters: &[Filter]) {
         for filter in filters {
-            self.filter(filter);
+            self.filter(*filter);
         }
-    }
-}
-
-impl Cell {
-    pub fn mask(&mut self, mask: Mask) {
-        *self &= mask
     }
 }
