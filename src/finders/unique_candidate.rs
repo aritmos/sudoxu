3f586bb5ec7a5bb3002b5f20f8615ec7e1ae8aff
Sebastian <52433836+aritmos@users.noreply.github.com>
2023-10-26 02:10:13 +0200

    Remove `section_select` in `grid.unique_index`
    Selecting Sections is not really useful yet, it will only be the case when dealing with the filter/finder for subsets of size 1.

--- src/finders/unique_candidate.rs
+++ src/finders/unique_candidate.rs
@@ -46,29 +46,18 @@ impl Grid {
     ///
     /// `sections`' `[bool; 3]` represents selecting the `Cell`'s row, column and box
     /// respectively to be tested against for unique candidates.
-    ///
-    /// Ability to opt-out of `Section`s allows to optimize repeated calls to the function.
-    pub fn unique_candidate(
-        &self,
-        grid_idx: GridIdx,
-        section_select: [bool; 3],
-    ) -> Result<Option<Num>, CandidateError> {
+    pub fn unique_candidate(&self, grid_idx: GridIdx) -> Result<Option<Num>, CandidateError> {
         let idx_info = grid_idx.associated_idxs();
 
         let mut result = Ok(None);
 
-        for (section_kind, section_idx, inner_idx) in section_select
-            .into_iter()
-            .zip([Row, Column, Box])
-            .zip(idx_info)
-            .filter_map(|((s, k), (si, ii))| s.then_some((k, si, ii)))
+        for (section_kind, (section_idx, inner_idx)) in [Row, Column, Box].into_iter().zip(idx_info)
         {
             let section_info = SectionInfo::new(section_kind, section_idx);
             let section = self.section(section_info);
             let section_result = section.unique_candidate(inner_idx);
             match (result, section_result) {
-                (Ok(None), Ok(None)) => (),
-                (Ok(Some(_)), Ok(None)) => (),
+                (_, Ok(None)) => (),
                 (Ok(None), Ok(Some(_))) => result = section_result,
                 (Ok(Some(a)), Ok(Some(b))) if a == b => (),
                 _ => return Err(CandidateError::MultipleUniqueCandidates),
