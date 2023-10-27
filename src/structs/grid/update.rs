1259fc11619dd3e148bae602051915d73cec6347
Sebastian <52433836+aritmos@users.noreply.github.com>
2023-10-27 19:01:04 +0200

    Update README

---
+++ src/structs/grid/update.rs
@@ -0,0 +1,10 @@
+use crate::structs::{cell::CandidateError, idx::GridIdx};
+
+use super::Grid;
+
+impl Grid {
+    /// Update the neighbours (all remaining `Cell`s in each `Section`) once a `Cell` is known to have a value.
+    pub fn known_cell_update_neighbours(&mut self, grid_idx: GridIdx) {
+        todo!()
+    }
+}
